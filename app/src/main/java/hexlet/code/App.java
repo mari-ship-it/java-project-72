12ea0f94be837e7baa5e632c44634744ebda6688
Marina Sirenko <msirenko96@gmail.com>
2025-08-09 20:16:07 +0300

    update App.java

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,27 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getApp() throws Exception {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return  app;
+    }
+
+    public static void main(String[] args) throws Exception {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+}
