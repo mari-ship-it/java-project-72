a08d49c01654ba5ae1f2f477ad552cc2458edfd2
Marina Sirenko <msirenko96@gmail.com>
2025-08-09 19:22:03 +0300

    add build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    application
+    jacoco
+    checkstyle
+    id ("org.sonarqube") version "6.2.0.5505"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+sonar {
+    properties {
+        property ("sonar.projectKey", "mari-ship-it_java-project-72")
+        property ("sonar.organization", "mari-ship-it")
+        property ("sonar.host.url", "https://sonarcloud.io")
+    }
+}
