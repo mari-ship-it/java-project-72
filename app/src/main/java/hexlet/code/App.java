ec813b51c5138090528b5da0890d9b6f56fa05f4
Marina Sirenko <msirenko96@gmail.com>
2026-02-01 14:06:04 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,6 +5,8 @@ import com.zaxxer.hikari.HikariDataSource;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.dto.UrlsPage;
+import hexlet.code.model.Url;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
@@ -12,10 +14,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 @Slf4j
 public class App {
@@ -63,12 +69,29 @@ public class App {
             config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
+        });
+
         app.get("/", ctx -> ctx.render("index.jte"));
+
+        app.get("/urls", ctx -> {
+            var term = ctx.queryParam("term");
+            ArrayList<Url> urls;
+
+            if (term != null) {
+//                urls = ;
+            } else {
+//                urls = ;
+            }
+//            var page = new UrlsPage(urls, term);
+//            ctx.render("urls/index.jte", model("page", page));
+        });
 
         return app;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception, SQLException {
         var app = getApp();
 
         app.start(getPort());
