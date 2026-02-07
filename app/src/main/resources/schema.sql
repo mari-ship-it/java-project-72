aaa257b3c524319f1ee1f6511de5fda74e4545a2
Marina Sirenko <msirenko96@gmail.com>
2026-02-08 00:36:40 +0300

    create table url_checks

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -5,3 +5,16 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL UNIQUE ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    url_id INT NOT NULL,
+    status_code INT NOT NULL,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (url_id) REFERENCES urls(id)
+);
